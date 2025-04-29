88c70774e8559a61e42f92a706e43c0c044bbdeb
juanlozanomotta8 <juanlozanomotta8@gmail.com>
2025-04-29 19:27:10 +0000

    Arreglo de boton  navbar

--- src/componentes/navbar.jsx
+++ src/componentes/navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className="w-full flex justify-center pt-8 fixed top-0 left-0 z-10">
+    <div className="w-full flex justify-center pt-8 fixed top-0 left-0 z-50">
       <nav className="flex items-center justify-between w-[50%] max-w-6xl border-2 border-black rounded-md px-8 py-3 bg-white shadow-md overflow-visible">
         
         {/* Logo */}
@@ -22,7 +22,7 @@ const Navbar = () => {
           className="border-2 border-black bg-[#A4FF00] text-black font-bold text-sm px-6 py-2 rounded-md
                      shadow-[0px_6px_0px_0px_black] transition-all duration-150 ease-in-out
                      hover:bg-gray-100 hover:scale-95 hover:translate-y-1 hover:shadow-[0px_2px_0px_0px_black]
-                     active:translate-y-2 active:shadow-[0px_1px_0px_0px_black]"
+                     active:translate-y-2 active:shadow-[0px_1px_0px_0px_black] "
         >
           Launch
         </button>
