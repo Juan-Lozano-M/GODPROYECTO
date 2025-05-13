0980dcb58f3086b97711ea1c48189c89848ad0f8
camilogo162 <camilogo162@gmail.com>
2025-05-13 18:20:29 +0000

    acomodado xl tamaño

--- src/componentes/SeccionF.jsx
+++ src/componentes/SeccionF.jsx
@@ -66,8 +66,8 @@ function SeccionF() {
           transition: 'transform 0.4s ease-out, opacity 0.4s ease-out',
         }}
       >
-        <p className="text-5xl lg:text-3xl 2xl:text-5xl">Unete A</p>
-        <p className="bg-[#A4FF00] p-4 lg:text-3xl 2xl:text-5xl rounded-md text-5xl">game of dreams</p>
+        <p className="text-5xl lg:text-3xl xl:text-4xl 2xl:text-5xl">Unete A</p>
+        <p className="bg-[#A4FF00] p-4 lg:text-3xl xl:text-4xl 2xl:text-5xl rounded-md text-5xl">game of dreams</p>
         <p className="mt-4 text-lg 2xl:text-2xl lg:text-base">Miles de estudiantes ya están</p>
         <p className="text-lg 2xl:text-2xl lg:text-base">transformando su futuro.</p>
 
