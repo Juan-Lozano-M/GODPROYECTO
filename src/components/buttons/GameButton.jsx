87840638b4d577f3830dd28e0ae1ff3ceeda624a
Michael_ara_905 <michael_jaramilloga@soy.sena.edu.co>
2025-06-09 17:38:13 -0500

    Ajustes Game Button

--- src/components/buttons/GameButton.jsx
+++ src/components/buttons/GameButton.jsx
@@ -4,20 +4,12 @@ const GameButton = ({
   text = "Enviar", 
   buttonClassName = "bg-white", 
   icon = null,
-<<<<<<< HEAD
-  onClick // Ensure this prop is accepted
-=======
   onClick 
->>>>>>> 4763ae3939448551830b859e67e1709d6836fb55
 }) => {
   return (
     <div className="flex">
       <button
-<<<<<<< HEAD
-        onClick={onClick} // Ensure the onClick prop is used here
-=======
       onClick={onClick}
->>>>>>> 4763ae3939448551830b859e67e1709d6836fb55
         className={`flex items-center justify-center gap-2 font-montserrat text-[14px] sm:text-xl font-bold border-2 border-black rounded-md px-2 py-1 shadow-[5px_5px_0px_black] cursor-pointer transition-all duration-300 ease-in-out hover:shadow-none hover:translate-x-1 hover:translate-y-1 ${buttonClassName}`}
       >
         {text}
