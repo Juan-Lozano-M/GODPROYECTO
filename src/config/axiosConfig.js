a94dc0b8927f7a4da21715972bad9a7e517dac90
Juan Martín Lozano Motta <Juanlozanomotta8@gmail.com>
2025-08-25 12:04:34 -0500

    Cambios para local

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://godbackend-production.up.railway.app/',
+  baseURL: 'http://127.0.0.1:5000',
   headers: {
     'Content-Type': 'application/json',
   },
